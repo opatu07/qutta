26800014b8cd62b35d6e3e078e43bb1961a62c56
ニシ <bunsekimal@gmail.com>
2024-01-04 09:19:59 +0900

    更新処理ができないArticleController.php

--- ArticleController.php
+++ ArticleController.php
@@ -8,12 +8,14 @@ use App\Models\Article;
 
 
 class ArticleController extends Controller
-{
+{   
+    // 画面にリダイレクト 必要性がわからない
     public function add()
     {
-        return view('/crud.add');
+        return view('crud.add');
     }
 
+    // 登録処理
     public function create(Request $request)
     {
         $article = new Article;
@@ -24,19 +26,36 @@ class ArticleController extends Controller
         return redirect('/post');
     }
 
+    // 一覧処理
     public function read(Request $request) {
         $data = article::all();
 
-        return view('crud/read')->with(['data' => $data]);
+        return view('crud.read', compact('data'));
     }
 
-    public function update(Request $request)
+    // 詳細画面
+    public function show($id)
     {
-
-    }
-
-    public function destory(Request $request)
-    {
+        $data = Article::find($id);
         
+        
+        return view('crud.show', compact('data'));
     }
+
+    // 編集処理
+    public function edit($id)
+    {
+        $data = Article::find($id);
+
+        return view('crud.edit', compact('data'));
+    }
+
+    // 更新処理
+    public function update(Request $request, $id)
+    {
+        $data = Article::find($id);
+        $updateArticle = $this->data->updateArticle($request, $data);
+        return redirect()->route('crud.read');
+    }
+
 }
