61df07612de93063f65a20317e69be7d165727f2
opatu07 <monogram1146@gmail.com>
2024-01-20 16:14:07 +0900

    ArticleController.phpを登録する

--- ArticleController.php
+++ ArticleController.php
@@ -3,49 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Article;
+use App\Models\Cook;
 
 
 
 class ArticleController extends Controller
 {   
-    // 画面にリダイレクト 必要性がわからない
+    // 登録画面
     public function add()
     {
         return view('crud.add');
     }
 
-    // 登録処理
-    public function create(Request $request)
+    public function __construct()
     {
-        $article = new Article;
-        $article->user_id = $request->user_id;
-        $article->title = $request->title;
-        $article->content = $request->content;
-        $article->save();
-        return redirect('/post');
+        $this->data = new Cook();
     }
 
-    // 一覧処理
-    public function read(Request $request) {
-        $data = article::all();
+    // 登録処理
+    public function create(Request $request)
+    {   
+        $registerCooks = $this->data->insertCooks($request);
+        return view('crud.add');
+    }
 
-        return view('crud.read', compact('data'));
+
+    // 一覧処理
+    public function read() {
+        $datas = $this->data->findAllCooks();
+        
+        return view('crud.read', compact('datas'));
     }
 
     // 詳細画面
     public function show($id)
     {
-        $data = Article::find($id);
-        
+        $data = Cook::find($id);
         
         return view('crud.show', compact('data'));
     }
 
-    // 編集処理
+    // 編集画面の表示
     public function edit($id)
     {
-        $data = Article::find($id);
+        $data = Cook::find($id);
 
         return view('crud.edit', compact('data'));
     }
@@ -53,9 +54,41 @@ class ArticleController extends Controller
     // 更新処理
     public function update(Request $request, $id)
     {
-        $data = Article::find($id);
-        $updateArticle = $this->data->updateArticle($request, $data);
-        return redirect()->route('crud.read');
+        $datas = Cook::find($id);
+        $updateCooks = $this->data->updateCooks($request, $datas);
+        return view('crud.read', compact('datas'));
+
     }
+
+    //削除処理
+    public function destroy($id)
+    {
+        $datas = $this->data->deleteCookById($id);
+
+        return view('crud.read', compact('datas'));
+    }
+
+    // 画像登録処理
+    public function store(Request $request)
+    {
+        //新規postを作成
+        $post= new cook();
+
+        //バリデーションルール
+        $inputs = request()->validate([
+            'image'=>'image'
+        ]);
+
+        //画像ファイルの保存場所指定
+        if(request('image')){
+            $filename=request()->file('image')->getClientOriginalName();
+            $inputs['image']=request('image')->storeAs('public/storage', $filename);
+        }
+
+        // postを保存
+        $post->create($inputs);
+
+    }
+
 
 }
